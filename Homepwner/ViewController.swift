eebd4048d098fc49a096a8f9cc1e9828993e965e
Paquin, Wyatt A <001382793@n101-a7-mac.local>
2018-02-13 20:21:53 -0500

    Initial Commit

---
+++ Homepwner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Paquin, Wyatt A on 2/13/18.
+//  Copyright © 2018 Paquin, Wyatt A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
