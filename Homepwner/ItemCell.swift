e2d7c1d677bdea0e471768dcd341a80dce1357d0
Wyatt Paquin <wyattpaquin@me.com>
2018-02-21 20:37:32 -0500

    started 12.0

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Wyatt Paquin on 2/21/18.
+//  Copyright © 2018 Paquin, Wyatt A. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+class ItemCell: UITableViewCell {
+    
+}
